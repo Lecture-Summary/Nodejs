58f9f2a820a09d30b8438c3accc8d28960b40d80
HoJin <kiss0104040@naver.com>
2019-12-27 23:55:59 +0900

    POST 방식으로 전송된 데이터 받기

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var http = require("http");
 var fs = require("fs");
 var url = require("url");
+var qs = require("querystring");
 
 function templateHTML(title, list, body) {
   return `
@@ -68,14 +69,14 @@ var app = http.createServer(function(request, response) {
         });
       });
     }
-  } else if (pathname == "/create") {
+  } else if (pathname === "/create") {
     fs.readdir("./data", function(error, filelist) {
       var title = "WEB - create";
       var list = templateList(filelist);
       var template = templateHTML(
         title,
         list,
-        `<form action="http://localhost:3000/process_create" method="post">
+        `<form action="http://localhost:3000/create_process" method="post">
          <p><input type="text" name="title" placeholder="title"/></p>
          <p>
           <textarea name="description" placeholder="description"></textarea>
@@ -88,6 +89,19 @@ var app = http.createServer(function(request, response) {
       response.writeHead(200);
       response.end(template);
     });
+  } else if (pathname === "/create_process") {
+    var body = "";
+    request.on("data", function(data) {
+      body += data;
+    });
+    request.on("end", function() {
+      var post = qs.parse(body);
+      var title = post.title;
+      var description = post.description;
+      console.log(post.title);
+    });
+    response.writeHead(200);
+    response.end("success");
   } else {
     response.writeHead(404);
     response.end("Not found");
